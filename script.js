69450d8692a97af757ffd88be51e8d56cd60f7ef
Abhay Chandra <abhaychandra7360@gmail.com>
2025-04-16 22:21:28 +0530

    add more functionality to the control bar and did some UI improvement

--- script.js
+++ script.js
@@ -11,23 +11,22 @@ async function getSong() {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
             songs.push(element.href);
-            // .split("/songs/")[1]).split(".mp3")[0]
         }
     }
     return songs
 }
 
 async function main() {
+    let currentSong = new Audio();
 
     let songs = await getSong();
-    console.log(songs);
-    let audio = new Audio(songs[1]);
+    // console.log(songs);
 
 
     let SongUl = document.querySelector(".song-list");
     let html = "";
-    for (const song of songs) {
-        html += `<div class="song flex align-center">
+    songs.forEach((song, index) => {
+        html += `<div class="song flex align-center" id ="${index}">
                         <svg width="8%" height="50%" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"
                             fill="#b3b3b3">
                             <path
@@ -48,21 +47,82 @@ async function main() {
                             <li>Abhay</li>
                         </ul>
                     </div>`;
-    }
+    });
     SongUl.innerHTML = html;
+
+    let index = -1;
+
+    const songDivs = document.querySelectorAll(".song");
+    songDivs.forEach(div => {
+        div.addEventListener("click", () => {
+            index = div.id;
+            currentSong.src = songs[index];
+            currentSong.play();
+            toggle.innerHTML = `
+            <svg width="24" height="24" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#000000">
+                <path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"/>
+            </svg>
+
+            `;
+            document.querySelectorAll(".song").forEach(card => card.classList.remove("playing"));
+            div.classList.add("playing");
+        });
+    });
+
+    
+    
+    
+    const previous = document.querySelector(".previous");
+    
+    previous.addEventListener("click", () => {
+        
+        index = (index - 1 + songs.length) % songs.length;
+        currentSong.src = songs[index];
+        currentSong.play();
+        
+        document.querySelectorAll(".song").forEach(card => card.classList.remove("playing"));
+        document.getElementById(index).classList.add("playing");
+    });
+
+    const next = document.querySelector(".next");
+
+    next.addEventListener("click", () => {
+        
+        index = (index + 1) % songs.length;  // loop to start if at end
+        currentSong.src = songs[index];
+        currentSong.play();
+        
+        document.querySelectorAll(".song").forEach(card => card.classList.remove("playing"));
+        document.getElementById(index).classList.add("playing");
+    });
+
+    let audio = currentSong;
 
     document.getElementById("toggle").addEventListener("click", () => {
         if (audio.paused) {
             audio.play();
+            toggle.innerHTML = `
+            <svg width="24" height="24" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#000000">
+                <path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"/>
+            </svg>
+            
+            `;
         } else {
             audio.pause();
+            toggle.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="#000000">
+                        <path
+                            d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z" />
+                    </svg>
+
+            `;
         }
     });
 
+
+
     audio.addEventListener("loadeddata", () => {
         let duration = audio.duration;
-        console.log(duration);
-
 
         let mins = Math.floor(duration / 60);
         let secs = duration % 60;
